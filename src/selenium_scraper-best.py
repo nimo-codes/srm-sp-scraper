de607dfd0d6f590293fb712b5465c81e8951ac9a
Naman nanda <64105119+nimo-codes@users.noreply.github.com>
2023-07-13 23:21:39 +0530

    v-3.5

--- src/selenium_scraper-best.py
+++ src/selenium_scraper-best.py
@@ -12,7 +12,7 @@ import logging
 
 ###########################################  Chrome Options  #################################################################################
 options = Options()
-# options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--start-maximized")
 options.add_argument("--window-size=1920,1080")
 options.add_argument("--disable-blink-features=AutomationControlled")
@@ -51,21 +51,59 @@ def convert(captcha_loc):
   gray = img.convert('L')
   gray.save('/Users/jarvis/pymycod/automation/test folder/captcha_gray.png')
   captcha_solved= pytesseract.image_to_string(gray)
-  tt(1)
   return captcha_solved[0:-1]
   
 def login(captcha_loc):
   login = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="login"]')))
   login.click()
+  login.clear()
   login.send_keys("nn4489")
   pass1 = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="passwd"]')))
   pass1.click()
+  pass1.clear()
   pass1.send_keys(keyring.get_password("srmsp","nn4489"))
   cap= wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="ccode"]')))
   cap.click()
   cap.send_keys(convert(captcha_loc))
+  tt(1)
   button1= wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="login_form"]/button')))
   button1.click()
+
+def cal_grades():
+  grade_points = {
+  "O": 10,
+  "A+": 9,
+  "A": 8,
+  "B+": 7,
+  "B": 6,
+  "C": 5,
+  "P": 4,
+  "F": 0,
+  "Ab": 0,
+  "*": 0,
+}
+  grades_list = []
+  credits_list = []
+  grade_pts = []
+  tot_grade_credit = 0
+  tot_credits = 0
+  grades= wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="listId24"]')))
+  grades.click()
+  name = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="divMainDetails"]/div/div/div/div/div[2]/div[2]/table')))
+  gradess = str(name.text)
+  list1 = gradess.splitlines()
+  for i in range(1,len(list1)):
+        each = list1[i].split(" ")
+        if each[-3].isdigit():
+            grades_list.append(each[-2])
+            credits_list.append(each[-3])
+  for i in range(0,len(grades_list)):
+    grade_pts.append(grade_points[grades_list[i]])
+  for i in range(0,len(grades_list)):  
+    tot_grade_credit += int(grade_pts[i])*int(credits_list[i])
+  for i in credits_list:
+    tot_credits+= int(i)
+  return "{:.2f}".format(tot_grade_credit/tot_credits)
   
 def err(reason,num):
   place = {0:"captcha can't be located",1:"pytesseract gave error",2:"work being handled"}
@@ -82,14 +120,10 @@ def err(reason,num):
 
 
 ###########################################  Start of code  #################################################################################
-try: 
-  captchaaa = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="login_form"]/div[4]/div[2]/img')))
-except Exception as error0:
-  err(error0,0)
-
 flag = False
 while flag==False:
   try:
+    captchaaa = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="login_form"]/div[4]/div[2]/img')))
     login(captchaaa)
     try:
       grades= wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="listId24"]')))
@@ -98,16 +132,11 @@ while flag==False:
       flag=False
   except Exception as error1:
     err(error1,1)
-  
+
+
 if flag==True:
   try:
-    grades= wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="listId24"]')))
-    print(grades)
-    # grades.click()
-    # # while: 
-    # name = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="divMainDetails"]/div/div/div/div/div[2]/div[2]/table')))
-    # name = driver.find_elements((By.XPATH,'//*[@id="divMainDetails"]/div/div/div/div/div[2]/div[2]/table'))
-    # # print(name)
+    print(cal_grades())
   except Exception as error2:
     err(error2,2)
 
